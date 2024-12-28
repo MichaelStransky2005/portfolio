c8d35755e8895932022a75488ead8a82f877ecd9
stranmi20 <stranmi20@sps-prosek.cz>
2024-12-28 17:43:15 +0100

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
 export default function Home() {
   return (
-   <main>
-
-   </main>
+    <main className="relative w-screen h-screen overflow-hidden">
+      <div className="stars absolute top-0 left-0 w-full h-full z-0"></div>
+      <div className="twinkling absolute top-0 left-0 w-full h-full z-0"></div>
+      <div className="relative z-10 flex items-center justify-center h-full">
+        <h1 className="text-white/50 text-4xl">Michael Stránský</h1>
+      </div>
+    </main>
   );
 }
